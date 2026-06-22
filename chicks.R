ce9fd41d9fb1885c8c189f7c7d370501f6e70efd
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2026-06-22 21:07:00 +0100

    subsetting

--- chicks.R
+++ chicks.R
@@ -7,3 +7,27 @@ mean(chicks$weight, na.rm = T)
 chicks[chicks$feed == 'casein', ]
 
 # remove NA at source
+which(!is.na(chicks$weight))
+chicks <- chicks[!is.na(chicks$weight),]
+chicks
+
+# another way -- subsetting
+chicks <- subset(chicks, !is.na(weight)) # chicks is assumes
+
+soybean_chicks <- subset(chicks, feed == 'soybean')
+soybean_chicks
+
+#####
+
+chicks
+rownames(chicks)
+# we've removed row 7 with the filter
+# we should reset the rownames to make them contiguous
+rownames(chicks) <- NULL
+rownames(chicks)
+
+####
+
+sum(chicks$feed == 'soybean')
+
+### lesson 2 / 1.04 hrs
